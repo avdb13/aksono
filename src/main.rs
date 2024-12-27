7c08b7d824e63f8777f9d2d94621db2a6cb021d5
avdb13 <avdb@keemail.me>
2024-12-27 15:01:20 +0000

    feat: introduce routing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::{convert::Infallible, process::ExitCode};
 
+use axum::Router;
+use tokio::net::TcpListener;
+
+use tracing::info;
+
 mod config;
 
-fn main() -> ExitCode {
-    match try_main() {
+#[tokio::main]
+async fn main() -> ExitCode {
+    match try_main().await {
         Ok(_) => ExitCode::SUCCESS,
         Err(error) => {
             eprintln!("\n{error:?}");
@@ -13,18 +19,34 @@ fn main() -> ExitCode {
     }
 }
 
-fn try_main() -> Result<(), Infallible> {
+async fn try_main() -> Result<(), Infallible> {
     tracing_subscriber::fmt()
         .event_format(tracing_subscriber::fmt::format().compact())
         .init();
 
-    let _config: config::Config = {
+    let config: config::Config = {
         let file = std::fs::read_to_string("./aksono.toml")
             .map_err(|_error| todo!())
             .unwrap();
 
         toml::from_str(&file).map_err(|_error| todo!()).unwrap()
     };
+
+    info!(
+        address = %config.listener.ip(),
+        port = %config.listener.port(),
+        "serving application"
+    );
+
+    let listener = TcpListener::bind(&*config.listener)
+        .await
+        .map_err(|_error| todo!())
+        .unwrap();
+
+    axum::serve(listener, Router::new())
+        .await
+        .map_err(|_error| todo!())
+        .unwrap();
 
     Ok(())
 }
